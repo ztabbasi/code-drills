8d16313a1ada10f341ac1e0b2b8b22a7438be4f8
Zia Abbasi <ztabbasi2003@yahoo.com>
2018-07-31 13:13:07 -0700

    add week one and two directories

---
+++ week-02/challenge-prompt.js
@@ -0,0 +1,149 @@
+//Variables
+//the nouns of programming
+
+//this is a comment - any code commented out is not run -
+//meaning that any code or text commented out is ignored.
+
+/*
+this is a multiline comment
+You can coment out many lines at once.
+For the most part you will see single line comments
+but it is good to see this.
+As in single line comments, any code contained within multi-line comments are not run -
+meaning that any code commented out is ignored.
+*/
+
+//Topic: strings
+
+//create a variable named subject with no value:
+//example: var hobby;
+
+
+
+//assign a value of "Programmers" to the variable subject
+//example: hobby = "problem solving"
+
+
+
+// Create a variable, firstName, and assign it a value of an empty string.
+//example: var emptyString = "";
+
+
+
+
+//assign a value of "Ada" to the variable firstName
+
+
+
+// Create a variable, lastName, and assign it a value of a string, "Lovelace".
+
+
+// Create a variable, fullName, and assign it a value of the combination of firstName and lastName with a space.
+
+
+
+// Create a variable, profession, and assign it a value of a string, "Computer Programmer".
+
+
+
+// Create a variable, knownFor, and assign it a value of a string, "first computer programmer".
+
+
+
+// Create a variable, firstAlgorithm, and assign it a value of a string, "Analytical Engine".
+
+
+
+// Create a variable, cityLocation, and assign it a value of a string, "London";
+
+
+
+// Create a variable, countryLocation, and assign it a value of a string, "England";
+
+
+
+// Create a variable, nationality, and assign it a value of a string, "British";
+
+
+
+//Topic: integers
+
+// Create a variable, birthYear, and assign it a value of a integer, 1815.
+
+
+
+// Create a variable, deathYear, and assign it a value of a integer, 1852.
+
+
+
+// Create a variable, ageAtPassing, and assign it a value of deathYear minus birthYear.
+
+
+
+// Create a variable, yearOfPublish, and assign it a value of 1842.
+
+
+
+// Create a variable, ageAtPassing, and assign it a value of deathYear minus birthYear.
+
+
+
+//Topic: console log
+
+////console log: "First Name:" + variable firstName
+
+
+
+////console log: "Last Name: Lovelace"
+
+
+
+////console log: "Profession: Computer Programmer"
+
+
+
+////console log: "BirthYear: 1815"
+
+
+
+//Topic: Concat Values
+
+// Create a variable, statementOne, and assign it a value of a string:
+// "Programmers: Ada Lovelace is a British Computer Programmer born in 1815."
+
+
+
+// Create a variable, statementTwo, and assign it a value of a string:
+//"She is commonly referred to as the first computer programmer"
+
+
+
+
+// Create a variable, statementThree, and assign it a value of a string:
+//"In 1842 she published the first Algorithm, the Analytical Engine, at the age of 27."
+
+
+
+// Create a variable, statementFour, and assign it a value of a string:
+//She was a British Citizen who lived in London, England until her passing in 1852 at the age of 37.
+
+
+
+//Topic: console.log()
+
+
+//console log Statement One
+
+
+
+//console log Statement Two
+
+
+
+//console log Statement Three
+
+
+
+//console log Statement Four
+
+
