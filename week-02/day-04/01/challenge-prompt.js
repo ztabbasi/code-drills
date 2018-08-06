98e30f7fcdb794bc3111c59dd269ccf3845a54ce
Zia Abbasi <ztabbasi2003@yahoo.com>
2018-08-05 22:33:20 -0700

    code drills week 2

---
+++ week-02/day-04/01/challenge-prompt.js
@@ -0,0 +1,121 @@
+// This line of code ensures our javascript doesn't run until the DOM is fully loaded.
+$(document).ready(function () {
+    
+    //As you saw in index.html, each letter of the alphabet can be found inside of an h3 tag with a class unique from the other letters.
+    //Create a click listener for each letter. Have each click event `append` the clicked letter to the div with the id of text-div.
+   
+    // $( "#target" ).click(function() {
+    //     alert( "Handler for .click() called." );
+    //   });
+
+    //   <h3 class="letterA"> A </h3>
+    // write a onclick listener of .letterA 
+    $("#letterA").click(function()  {
+        alert("letter A has been clicked");
+    });
+        // append the letter A on the class of text-div
+        
+    // write a onclick listener of .letterB 
+    $("#letterB").click(function()  {
+        alert("letter B has been clicked");
+    });
+    
+        // append the letter B on the class of text-div
+        
+    // write a onclick listener of .letterC
+    
+        // append the letter C on the class of text-div
+        
+    // write a onclick listener of .letterD 
+        
+        // append the letter D on the class of text-div
+        
+    // write a onclick listener of .letterE 
+        
+        // append the letter E on the class of text-div
+        
+    // write a onclick listener of .letterF 
+        
+        // append the letter F on the class of text-div
+        
+    // write a onclick listener of .letterG 
+        
+        // append the letter G on the class of text-div
+        
+    // write a onclick listener of .letterH 
+        
+        // append the letter H on the class of text-div
+        
+    // write a onclick listener of .letterI 
+        
+        // append the letter I on the class of text-div
+        
+    // write a onclick listener of .letterJ 
+        
+        // append the letter J on the class of text-div
+        
+    // write a onclick listener of .letterK 
+        
+        // append the letter K on the class of text-div
+        
+    // write a onclick listener of .letterL 
+        
+        // append the letter L on the class of text-div
+        
+    // write a onclick listener of .letterM 
+        
+        // append the letter M on the class of text-div
+        
+    // write a onclick listener of .letterN 
+        
+        // append the letter N on the class of text-div
+        
+    // write a onclick listener of .letterO 
+        
+        // append the letter O on the class of text-div
+        
+    // write a onclick listener of .letterP 
+        
+        // append the letter P on the class of text-div
+        
+    // write a onclick listener of .letterQ 
+        
+        // append the letter Q on the class of text-div
+        
+    // write a onclick listener of .letterR 
+        
+        // append the letter R on the class of text-div
+        
+    // write a onclick listener of .letterS 
+        
+        // append the letter S on the class of text-div
+        
+    // write a onclick listener of .letterT 
+        
+        // append the letter T on the class of text-div
+        
+    // write a onclick listener of .letterU 
+        
+        // append the letter U on the class of text-div
+        
+    // write a onclick listener of .letterV 
+        
+        // append the letter V on the class of text-div
+        
+    // write a onclick listener of .letterW 
+        
+        // append the letter W on the class of text-div
+        
+    // write a onclick listener of .letterX 
+        
+        // append the letter X on the class of text-div
+        
+    // write a onclick listener of .letterY 
+        
+        // append the letter Y on the class of text-div
+        
+    // write a onclick listener of .letterZ 
+        
+        // append the letter Z on the class of text-div
+        
+});
---
+++ week-02/day-04/02/challenge-prompt.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+//In each instance, write your code directly below the question asking for it. 
+
+
+        //1. create a variable called text and have it equal an empty string
+
+        //2. create a function called randomizingText
+
+            //3. create a variable called possibleLetters set to a string containing the whole alphabet. For example possibleLetters = "ABCD..."
+
+            // 4. The text variable will continue to grow in length as the user clicks the question mark. Reassign the text variable to itself plus a new random letter. You will need to use the possibleLetters variable to accomplish this. Research the charAt() method if you're stuck.
+
+            //5. Finally have this function return the current value of the text variable
+
+    $(".jumbotron").on("click", "#randomLetter", function () {
+
+
+        // console.log the function randomizingText (don't forget to invoke the function)
+
+        // append the text variable to the h1 tag with the id of letters
+
+
+    });
+
+});
